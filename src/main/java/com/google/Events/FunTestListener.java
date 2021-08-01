d280a52918d20bf33a9b7ef0d8d04b4840634fec
Roosterseatyou <ltbkthree@icloud.com>
2021-07-31 19:53:28 -0500

    E

--- src/main/java/com/google/Events/FunTestListener.java
+++ src/main/java/com/google/Events/FunTestListener.java
@@ -1,17 +1,25 @@
 package com.google.Events;
 
+import com.google.Items.Hoes.ReplantingHoe;
 import com.google.Items.Hoes.TillingHoe;
+import com.google.Main;
 import com.google.Utils.Utils;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Ageable;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,10 +59,105 @@ public class FunTestListener implements Listener {
             }
         }
     }
+    @EventHandler
+    public void onPlayerHarvest(BlockBreakEvent e) {
+        Player p = e.getPlayer();
+        Block b = e.getBlock();
+
+        Material cropBlockMat = null;
+
+        if (b.getType() == Material.WHEAT) {
+            cropBlockMat = Material.WHEAT;
+        } else if (b.getType() == Material.POTATOES) {
+            cropBlockMat = Material.POTATOES;
+        } else if (b.getType() == Material.CARROTS) {
+            cropBlockMat = Material.CARROTS;
+        } else if (b.getType() == Material.BEETROOTS) {
+            cropBlockMat = Material.BEETROOTS;
+        } else if (b.getType() == Material.NETHER_WART) {
+            cropBlockMat = Material.NETHER_WART;
+        }
+        if (cropBlockMat != null && isFullyGrown(b)) {
+            Material seed = getSeeds(cropBlockMat);
+            if(isSeedInInv(p, cropBlockMat)){
+                Location loc = b.getLocation();
+                removeSeed(p, seed);
+                replantCrop(loc, cropBlockMat);
+            }
+        }
+    }
+
+    public void replantCrop(Location loc, Material seed){
+        Bukkit.getScheduler().runTaskLater(Main.getInstance(), () -> {
+            loc.getBlock().setType(seed);
+        }, 20L);
+    }
+
+    public void removeSeed(Player p, Material seedMat){
+        int seedIndexLocation = -1;
+        ItemStack currentItems;
+        PlayerInventory pInv = p.getInventory();
+
+        for(int slotIndex = 0; slotIndex <= pInv.getSize(); slotIndex++){
+            currentItems = pInv.getItem(slotIndex);
+            if(currentItems != null){
+                if(currentItems.getType() == seedMat){
+                    seedIndexLocation =slotIndex;
+
+                    slotIndex = pInv.getSize();
+                }
+            }
+        }
+
+        if(seedIndexLocation != -1){
+            ItemStack seedItemStack = pInv.getItem(seedIndexLocation);
+            if(seedItemStack != null){
+                int seedAmt = seedItemStack.getAmount();
+                seedItemStack.setAmount(seedAmt - 1);
+            }
+        }
+    }
+    public boolean isSeedInInv(Player p, Material mat){
+        PlayerInventory pInv = p.getInventory();
+        if (mat == Material.WHEAT) {
+            return pInv.contains(Material.WHEAT_SEEDS);
+        } else if (mat == Material.POTATOES) {
+            return pInv.contains(Material.POTATO);
+        } else if (mat == Material.CARROTS) {
+            return pInv.contains(Material.CARROT);
+        } else if (mat == Material.BEETROOTS) {
+            return pInv.contains(Material.BEETROOT);
+        } else if (mat == Material.NETHER_WART) {
+            return pInv.contains(Material.NETHER_WART);
+        }
+        return false;
+    }
+    public Material getSeeds (Material mat) {
+        if (mat == Material.WHEAT) {
+            return Material.WHEAT_SEEDS;
+        } else if (mat == Material.POTATOES) {
+            return Material.POTATO;
+        } else if (mat == Material.CARROTS) {
+            return Material.CARROT;
+        } else if (mat == Material.BEETROOTS) {
+            return Material.BEETROOT;
+        } else if (mat == Material.NETHER_WART) {
+            return Material.NETHER_WART;
+        }
+            return null;
+        }
+    public boolean isFullyGrown(Block b) {
+        Ageable ageable = (Ageable) b.getBlockData();
+        int maxAge = ageable.getMaximumAge();
+
+        return ageable.getAge() == maxAge;
+
+    }
     //Just need to get the item for testing lmao. Will remove after i decide on a recipe :)
     @EventHandler
     public void onPlayerShift(PlayerToggleSneakEvent e){
         Player p = e.getPlayer();
+        p.getInventory().addItem(ReplantingHoe.ReplantHoe);
         p.getInventory().addItem(TillingHoe.TillingHoe);
     }
 }
--- src/main/java/com/google/Main.java
+++ src/main/java/com/google/Main.java
@@ -1,22 +1,32 @@
 package com.google;
 
 import com.google.Events.FunTestListener;
+import com.google.Items.Hoes.ReplantingHoe;
 import com.google.Items.Hoes.TillingHoe;
 import org.bukkit.Material;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
+
+    public static Plugin instance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new FunTestListener(), this);
         TillingHoe.init();
+        ReplantingHoe.init();
+        instance = this;
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Plugin getInstance(){
+        return instance;
     }
 }
